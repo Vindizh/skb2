a700cab9ba160404afe3e381221b43f44113360c
Jakovenko Gleb <Jakovenko Gleb>
2016-11-12 22:14:49 +0300

    Second task

--- src/index.js
+++ src/index.js
@@ -21,6 +21,25 @@ app.get('/task2A', (req, res) => {
   return res.json(sum);
 });
 
+app.get('/task2B', (req, res) => {
+  let words = req.query.fullname.split(' ').reverse();
+  let result = words[0];
+
+  if (req.query.fullname.match(/[0-9]|_|\//) || !req.query.fullname || words.length > 3) {
+    return res.send('Invalid fullname');
+  }
+
+  if (words[2]) {
+    result += ' ' + words[2][0] + '.'
+  }
+
+  if (words[1]) {
+    result += ' ' + words[1][0] + '.'
+  }
+
+  return res.send(result);
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!'); // eslint-disable-line
 });
